6cb66edd8738620e765574cc2ad3a44953a91d03
michellenikeetha <michellenikeetha@gmail.com>
2024-11-18 21:34:16 +0530

    updated annoucements

--- src/components/AnnouncementsPage.jsx
+++ src/components/AnnouncementsPage.jsx
@@ -1,6 +1,6 @@
 // components/AnnouncementsPage.js
 import React from 'react';
-import Navbar from './Navbar';
+import SignedInNavbar from './SignedInNavbar';
 import AnnouncementCard from './AnnouncementCard';
 import { useNavigate } from 'react-router-dom';
 
@@ -29,7 +29,7 @@ const AnnouncementsPage = () => {
 
   return (
     <div>
-      <Navbar />
+      <SignedInNavbar />
       <div className="container mx-auto px-6 py-8">
         <h1 className="text-3xl font-bold mb-6">Announcements</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
